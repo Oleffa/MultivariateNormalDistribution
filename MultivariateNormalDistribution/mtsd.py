e6d6ecfedd0eb6c5d894a0e5f56a281c57d771a8
Oliver Struckmeier <oliver.struckmeier@aalto.fi>
2018-04-27 14:15:35 +0300

    code cleanup + digits datset works

--- MultivariateNormalDistribution/mtsd.py
+++ MultivariateNormalDistribution/mtsd.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import math
+from scipy.sparse import linalg as sla
 
 class MultivariateTStudentDistribution:
     # The Multivariate Student t distribution is a more robust alternative to the MVN. It is used in
@@ -17,16 +18,24 @@ class MultivariateTStudentDistribution:
         self.sigma = covariance
         self.nu = dof
     
+    # Getter and Setter for sigma, update __lambda and __sigma_det every time sigma is set
     @property
     def sigma(self):
-        
-        return self._sigma
+        return self.__sigma
     
     @sigma.setter
     def sigma(self, covariance):
-        self._sigma = covariance
-        self._lambda = np.linalg.inv(np.asarray(self._sigma))
-        self._sigma_det = np.linalg.det(self._sigma)
+        # Check if sigma_det is positive semidefinite if not find the closes positive semidefinite
+        try:
+            p = (np.linalg.cholesky(covariance))
+            self.__sigma = covariance
+        except np.linalg.linalg.LinAlgError:
+            self.__sigma = self.nearestSPD(covariance)
+            
+        #print("mtsd sigma: \n{}".format(self.__sigma))
+        self.__lambda = np.linalg.inv(np.asarray(self.__sigma))
+        #print("mtsd _lambda: \n{}".format(self.__lambda))
+        self.__sigma_det = np.linalg.det(self.__sigma)
         
     def pdf(self, data):
         # Murphy, 2.5.3
@@ -37,22 +46,53 @@ class MultivariateTStudentDistribution:
             prob = math.exp(self.logpdf(data))
         return prob
     def logpdf(self,data):
-        data = np.asarray(data)
         # Data is a DxN matrix, where D is dimensionality and N is number of points to evaluate
+        data = np.asarray(data)
+        N = np.shape(data)[1]
+        logprob = np.zeros((N,1))
         if len(data) is not self.D:
             print("Error: Dimensionality of data and MVST do not agree!")
-        else:
-            N = np.shape(data)[1]
-            logprob = np.zeros((N,1))
-            
+        else:            
             lognu = math.log(self.nu)
-            logdet = math.log(self._sigma_det)
+            logdet = math.log(self.__sigma_det)
             b = (self.nu+self.D)/2
             for i in range(0,N):
                     x1 = np.transpose(data[:,i].reshape(self.D,1) - self.mu)
                     x2 = (data[:,i].reshape(self.D,1) - self.mu)
-                    mahalanobis = np.matmul(np.matmul(x1,self._lambda),x2) # Mahalanobis distance is a distance from point to a distribution
+                    mahalanobis = np.matmul(np.matmul(x1,self.__lambda),x2) # Mahalanobis distance is a distance from point to a distribution
+                    mahalanobis = mahalanobis[0].real # Mahalanobis has an imaginary part sometimes
+                    mahalanobis_imaginary = mahalanobis[0].imag
                     z = math.log1p(mahalanobis/self.nu)
                     logprob[i] = math.lgamma(b) - math.lgamma(self.nu/2) - 0.5*logdet - \
                                  (self.D/2)*(lognu+math.log(math.pi)) - b*z
         return logprob
+    # This is needed to handle the error in which the sigma is not positive definite and the nearest positive definite matrix
+    # is generated: https://stackoverflow.com/questions/10939213/how-can-i-calculate-the-nearest-positive-semi-definite-matrix
+    # nearestSPD() helpers
+    def _getAplus(self,A):
+        eigval, eigvec = np.linalg.eig(A)
+        Q = np.matrix(eigvec)
+        xdiag = np.matrix(np.diag(np.maximum(eigval, 0)))
+        return Q*xdiag*Q.T
+
+    def _getPs(self, A, W=None):
+        W05 = np.matrix(W**.5)
+        return  W05.I * self._getAplus(W05 * A * W05) * W05.I
+
+    def _getPu(self, A, W=None):
+        Aret = np.array(A.copy())
+        Aret[W > 0] = np.array(W)[W > 0]
+        return np.matrix(Aret)    
+    def nearestSPD(self, A, nit=10):
+        n = A.shape[0]
+        W = np.identity(n) 
+        # W is the matrix used for the norm (assumed to be Identity matrix here)
+        # the algorithm should work for any diagonal W
+        deltaS = 0
+        Yk = A.copy()
+        for k in range(nit):
+            Rk = Yk - deltaS
+            Xk = self._getPs(Rk, W=W)
+            deltaS = Xk - Rk
+            Yk = self._getPu(Xk, W=W)
+        return Yk
--- MultivariateNormalDistribution/mvn.py
+++ MultivariateNormalDistribution/mvn.py
@@ -15,21 +15,26 @@ class MultivariateNormalDistribution:
         self.kappa0 = kappa # Weight for m0
         self.nu0 = nu # Weight of S0
         self.S0 = S0 # Prior mean for Sigma
-        if covariance is not None: self.sigma = covariance
-        self.isInitialized = False
-        
+        if covariance is not None:
+            self.sigma = covariance
+        # Variable to check if MAP or MLE updated the prior    
+        self.updatedPrior = False
+    
+    # Getter and Setter for sigma, update __lambda and __sigma_det every time sigma is set
     @property
     def sigma(self):
-        return self._sigma
+        return self.__sigma
     
     @sigma.setter
     def sigma(self, covariance):
-        self._sigma = covariance
+        # Handle the error if covariance is positive definit
         try:
-            self._lambda = np.linalg.inv(np.asarray(self._sigma))
-        except np.linalg.LinAlgError:
-            self._lambda = np.zeros((self.D, self.D))
-        self._sigma_det = np.linalg.det(self._sigma)
+            p = (np.linalg.cholesky(covariance))
+            self.__sigma = covariance
+        except np.linalg.linalg.LinAlgError:
+            self.__sigma = self.nearestSPD(covariance)
+        self.__lambda = np.linalg.inv(np.asarray(self.__sigma))
+        self.__sigma_det = np.linalg.det(self.__sigma)
     
     def pdf(self):
         # Data is a DxN matrix where D is dimensionality and N is number of points to evaluate
@@ -50,22 +55,23 @@ class MultivariateNormalDistribution:
             for i in range(0,N):
                 x1 = np.transpose(data[:,i].reshape(self.D,1) - self.mu)
                 x2 = (data[:,i].reshape(self.D,1) - self.mu)
-                asdf = np.matmul(np.matmul(x1,self._lambda),x2)
-                logprob[i] = -0.5*(np.log(self._sigma_det) + self.D*np.log(2*math.pi) + asdf)
+                # Creative variable naming
+                asdf = np.matmul(np.matmul(x1,self.__lambda),x2)
+                logprob[i] = -0.5*(np.log(self.__sigma_det) + self.D*np.log(2*math.pi) + asdf)
         return logprob
     def likelihood(self, data):
         # Murphy 4.6.3.1
         # p(D|mu,sigma)
-        tilde, N = np.shape(data)
+        tilde, N = np.shape(data) # Tilde is unused, like in matlab
         data_mean = np.average(data,axis=1).reshape(self.D, 1)
         scatter_matrix = np.zeros((self.D,self.D))
         for i in range (0,N):
             x = (data[:,i].reshape(self.D,1) - data_mean)
             scatter_matrix = scatter_matrix + np.matmul(x,np.transpose(x))
         dm_mu = data_mean-self.mu
-        exp1 = math.exp(-N*0.5*np.matmul(np.matmul(np.transpose(dm_mu),self._lambda),dm_mu))
-        exp2 = math.exp(-0.5*np.trace(np.matmul(self._lambda,scatter_matrix)))
-        lik = math.pow((2*math.pi),(-N*self.D*0.5)) * math.pow(self._sigma_det,(-N*0.5)) *exp1* exp2
+        exp1 = math.exp(-N*0.5*np.matmul(np.matmul(np.transpose(dm_mu),self.__lambda),dm_mu))
+        exp2 = math.exp(-0.5*np.trace(np.matmul(self.__lambda,scatter_matrix)))
+        lik = math.pow((2*math.pi),(-N*self.D*0.5)) * math.pow(self.__sigma_det,(-N*0.5)) *exp1* exp2
         return lik
     def loglikelihood(self, data):
         return np.sum(self.logpdf(data))
@@ -74,7 +80,7 @@ class MultivariateNormalDistribution:
         # data is a DxN matrix where D is dimensionality and N is the number of points for MLE
         data = np.asarray(data)
         self.D, N = np.shape(data)
-        mu = (np.sum(data,axis=1)/N).reshape(self.D,1)
+        mu = (np.divide(np.sum(data,axis=1),N)).reshape(self.D,1)
         incr = np.zeros((self.D, self.D))
 
         for i in range(0,N):
@@ -82,14 +88,15 @@ class MultivariateNormalDistribution:
             incr = incr + np.matmul(column,np.transpose(column))
 
         mu2 = mu*np.transpose(mu)
-        sigma = (incr/N)-mu2
+        sig = (incr/N)-mu2
         
         # Do mle without writing mu and sigma
         if update_flag is True:
             self.mu = mu
-            self.sigma = sigma
-            # sigma and mu are set
-            self.isInitialized = True
+            self.sigma = sig
+            # Set the flag that the prior has been updated (used to solve an error in the test applications with the digits dataset
+            self.updatedPrior = True
+        return
     def computePosterior(self,data, update_flag=True):
         # Murphy12, 4.6.3.3
         dim, N = np.shape(data)
@@ -107,7 +114,7 @@ class MultivariateNormalDistribution:
             S_N = self.S0 + uncentered_scatter_matrix + \
                     self.kappa0*np.matmul(self.m0,np.transpose(self.m0)) -\
                     kappa_N*np.matmul(m_N,np.transpose(m_N))
-            # Update prior values
+            # Update old prior values
             self.m0 = m_N
             self.kappa0 = kappa_N
             self.S0 = S_N
@@ -121,12 +128,11 @@ class MultivariateNormalDistribution:
         # MAP estimate (posterior mode)
         m_N, tilde, nu_N, S_N = self.computePosterior(data, update_flag)
         mu = m_N
-        sigma = np.divide(S_N,(nu_N + self.D + 2))
+        sig = np.divide(S_N,(nu_N + self.D + 2))
         if update_flag is True:
             self.mu = mu
-            self.sigma = sigma
-            # sigma and mu are set
-            self.isInitialized = True
+            self.sigma = sig
+            self.updatedPrior = True
         return
     
     def logPosteriorPredictive(self, data, update_flag=True):
@@ -134,6 +140,9 @@ class MultivariateNormalDistribution:
         # Uses m0, S0 and so on. Assuming that the computePosterior was overwriting the old prior
         dof = self.nu0 - self.D + 1;
         covariance = (self.kappa0 + 1) * self.S0/(self.kappa0*dof)
+        #print("mvn kappa0: {}".format(self.kappa0))
+        #print("mvn S0: {}".format(self.S0))
+        #print("mvn dpf: {}".format(dof))
         # The posterior predictive given by p(x|D) = p(x,D)/p(D) so it can easily be evaluated in terms
         # of a ratio of marginal likelihoods. It turns out that this ratio has the form of a MVST.
         tst = mtsd.MultivariateTStudentDistribution(self.D, self.m0, covariance, dof)
@@ -142,8 +151,40 @@ class MultivariateNormalDistribution:
     
     def sampleDistribution(self, N):
         # Generate a DxN matrix where D is dimensionality and N is number of samples
+        # Generate N random samples from this distribution
         samples = 0
         if N > 0:
-            samples = np.random.multivariate_normal(np.squeeze(self.mu), self.sigma, N).T
+            samples = np.random.multivariate_normal(np.squeeze(self.mu), self.__sigma, N).T
         return samples
+    
+    # This is needed to handle the error in which the sigma is not positive definite and the nearest positive definite matrix
+    # is generated: https://stackoverflow.com/questions/10939213/how-can-i-calculate-the-nearest-positive-semi-definite-matrix
+    # nearestSPD() helpers
+    def _getAplus(self,A):
+        eigval, eigvec = np.linalg.eig(A)
+        Q = np.matrix(eigvec)
+        xdiag = np.matrix(np.diag(np.maximum(eigval, 0)))
+        return Q*xdiag*Q.T
+
+    def _getPs(self, A, W=None):
+        W05 = np.matrix(W**.5)
+        return  W05.I * self._getAplus(W05 * A * W05) * W05.I
+
+    def _getPu(self, A, W=None):
+        Aret = np.array(A.copy())
+        Aret[W > 0] = np.array(W)[W > 0]
+        return np.matrix(Aret)    
+    def nearestSPD(self, A, nit=10):
+        n = A.shape[0]
+        W = np.identity(n) 
+        # W is the matrix used for the norm (assumed to be Identity matrix here)
+        # the algorithm should work for any diagonal W
+        deltaS = 0
+        Yk = A.copy()
+        for k in range(nit):
+            Rk = Yk - deltaS
+            Xk = self._getPs(Rk, W=W)
+            deltaS = Xk - Rk
+            Yk = self._getPu(Xk, W=W)
+        return Yk
     
